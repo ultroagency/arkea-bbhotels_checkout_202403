67e0e5354e2b99d252042e1458f5ab72a06e7379
Antoine Caillet <antoine.caillet@ultro.fr>
2024-03-27 09:36:26 +0100

    🔥 Remove CommerceLayer logo on 404

--- components/composite/ErrorContainer/index.tsx
+++ components/composite/ErrorContainer/index.tsx
@@ -8,9 +8,6 @@ export const ErrorContainer = ({ children }: { children: ChildrenType }) => {
     <Base>
       <Container>
         <Wrapper>
-          <LogoWrapper>
-            <FullLogo className="self-center text-black md:pl-4 md:self-auto" />
-          </LogoWrapper>
           <Main>
             <Error>{children}</Error>
           </Main>
